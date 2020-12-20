e368a69f66392bf55a8d3972510b84b7d2e6387e
Artem <arlaptiev@gmail.com>
2020-12-20 12:30:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Tex } from 'react-tex';
+import { InlineMath, BlockMath } from 'react-katex';
 import QuizContainer from './Containers/Quiz.container';
 import './App.css';
 import question from './Fixtures/Question';
@@ -8,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <QuizContainer {...question} />
+      <Tex texContent={'3 + 4 = 6 \\text{  -- рендеринг за допомогою Tex}'}/>
+      <BlockMath>{String.raw`\begin{Bmatrix}\text{кіт} & b \\c & d\end{Bmatrix} \text{-- рендеринг з KaTeX}`}</BlockMath>
     </div>
   );
 }
